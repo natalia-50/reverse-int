19f754a08060f8e7eeb344a9765e1068ddba964d
natalia-50 <79153214+natalia-50@users.noreply.github.com>
2021-03-15 15:11:12 +0300

    Update index.js
    add an option for negative numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+str = "" + Math.abs(n);
+    strr = "";
+    for(let i = 0; i < str.length; i++) {
+        strr = str[i] + strr;
 }
+    return (strr*1)
